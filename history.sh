50220044e0b44eb4477fe44eca5d7148f3e946ca
shaffar3 <shaffar3@illinois.edu>
2021-12-27 17:23:59 -0500

    finished history mode

--- history.sh
+++ history.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 echo "This mode will pick a random command in your bash history and perform it. This is very dangerous, only perform this in a vm. For example suppose you did rm -rf or something, that would be pretty devastating."
 
+echo
+
+echo
+
 echo "what is the full path of your history file (or any file with a list of commands). If left blank, it will default to ~/.bash_eternal_history, which is what it is on my computer"
 
 read file
 if [ -z "$file" ]
 then
-    file="~/.bash_eternal_history"
+    #file="~/.bash_eternal_history"
+    file=~/.bash_eternal_history
+    #file="dummy_history.txt"
 fi
-line_count=$()
-
-
-
+echo $file
+line_count=$( wc -l < $file )
+echo $line_count
 
 echo "cool will run command in:"
 echo "5 :>"
@@ -30,18 +35,12 @@ echo firing
 echo
 echo
 sleep 0.2
-random_int=$RANDOM
-random_num=$(($random_int % $den))
-if [[ "$random_num" -lt "$num" ]]
-then
-    echo "command $command will run unless you can stop it next 0.1 second lmao"
-    echo
-    sleep 0.1
-    #$command
-    echo
-    echo "R.I.P"
-else
-    echo "Today is your lucky day. You are spared"
-fi
+random_num=$(((($RANDOM % $line_count)) + 1))
+command=$(sed "${random_num}q;d" $file)
+
+echo $command
+#$command uncomment if your not a coward
+
+
 #$(( $RANDOM % 2 )) == 0 ] && ($command && echo "you win") || echo "You lose"
 #$command
